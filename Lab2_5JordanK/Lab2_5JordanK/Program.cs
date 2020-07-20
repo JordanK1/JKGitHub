a282492a256f763fde8dfda97c8bfdc48a694b12
Jordan Koeppl <blaisekoeppl@gmail.com>
2020-07-20 18:28:17 -0500

    Initial upload of four (4) LAB assignments

---
+++ Lab2_5JordanK/Lab2_5JordanK/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+
+namespace Lab2_5JordanK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Lab2_5
+            //1: Add Numbers
+            int num1; //blank input for first number
+            int num2; //blank input for second number
+
+            Console.WriteLine("Enter the first addition value: ");
+            string userInput = Console.ReadLine();
+            num1 = int.Parse(userInput);
+            Console.WriteLine("Enter the second addition value: ");
+            string userInput2 = Console.ReadLine();
+            num2 = int.Parse(userInput2); //Looks like this method means I have to make multiple userInput variables.
+
+            int result = num1 + num2;
+            Console.WriteLine("Your Sum is: ");
+            Console.WriteLine(result);
+
+
+            //Subtract, Multiply and Divide Numbers
+            int num3; //blank input for other equations
+            int num4; //blank input for other equations
+
+            Console.WriteLine("Enter the first value: ");
+            num3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the second value: ");
+            num4 = Convert.ToInt32(Console.ReadLine());
+
+            int result2 = num3 + num4;
+            Console.WriteLine("Your Result is: ");
+            Console.WriteLine(result2);
+
+            result2 = num3 * num4;
+            Console.WriteLine("Your Result is: ");
+            Console.WriteLine(result2);
+
+            double result3 = (double)num3 / (double)num4;
+            Console.WriteLine("Your Result is: ");
+            Console.WriteLine(result3);
+
+
+            //2: Hello (Your Name)
+            Console.WriteLine("What is your name?   ");
+            string nameInput = Console.ReadLine();
+            Console.WriteLine("Hello, " + nameInput);
+
+
+
+            //3: Multiplication of 3 numbers
+            int num5; //multiplication variable 1
+            int num6; //multiplication variable 2
+            int num7; //multiplication variable 3
+
+            Console.WriteLine("Enter number 1 of 3 for multiplying: ");
+            num5 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 2 of 3 for multiplying: ");
+            num6 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter number 3 ofr 3 for multiplying: ");
+            num7 = Convert.ToInt32(Console.ReadLine());
+
+            int result4 = num5 * num6 * num7;
+            Console.WriteLine("Your Product is: ");
+            Console.WriteLine(result4);
+
+
+            //4: Flattery.exe
+            Console.WriteLine("How old are you, " + nameInput + "?");
+            string ageInput = Console.ReadLine();
+            Console.WriteLine("Wow, I would never have guessed " + ageInput + "! Do all " + ageInput + " year-olds look as good as you?");
+        }
+
+
+    }
+}
---
+++ Lab3_4JordanK/Lab3_4JordanK/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+public class Example
+{
+    public static void Main()
+    {
+        //2A
+        string pattern = @"[d-e]";
+        string input = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions options = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(input, pattern, options))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+
+
+        //2B
+        string patternb = @"[d-e][*d]";
+        string inputb = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions optionsb = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(inputb, patternb, optionsb))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+
+
+        //2C
+        string patternc = @"[a][a-z][a]";
+        string inputc = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions optionsc = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(inputc, patternc, optionsc))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+
+
+        //2D
+        string patternd = @"[ ][p][a-z]*";
+        string inputd = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions optionsd = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(inputd, patternd, optionsd))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+
+
+        //2E
+        string patterne = @"[a-z]*[e][ ]";
+        string inpute = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions optionse = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(inpute, patterne, optionse))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+
+
+        //2F
+        string patternf = @"[A-Z]";
+        string inputf = @"Episode IV, A NEW HOPE It is a period of civil war. Rebel spaceships, striking from a hidden base, have won their first victory against the evil Galactic Empire. During the battle, Rebel spies managed to steal secret plans to the Empire’s ultimate weapon, the DEATH STAR, an armored space station with enough power to destroy an entire planet. Pursued by the Empire’s sinister agents, Princess Leia races home aboard her starship, custodian of the stolen plans that can save her people and restore freedom to the galaxy....";
+        RegexOptions optionsf = RegexOptions.Multiline;
+
+        foreach (Match m in Regex.Matches(inputf, patternf, optionsf))
+        {
+            Console.WriteLine("'{0}' found at index {1}.", m.Value, m.Index);
+        }
+    }
+}
+
+
