f6fb5c14fc0467fbc71f26e31134641454a75c0d
Jordan Koeppl <blaisekoeppl@gmail.com>
2020-07-20 19:02:31 -0500

    Reupload of file Lab2_4 to fix issue with first attempt

---
+++ Lab2_4JordanK/Lab2_4JordanK/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Lab2_4JordanK
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //HW Lab Practice by Jordan K.
+            
+            int myFirstNumber = 19;
+            int mySecondNumber = 244;
+            Console.WriteLine(myFirstNumber * mySecondNumber);
+            string myText = "Some Text";
+            Console.WriteLine(myText + mySecondNumber);
+            string moreText = " Goes Here";
+            Console.WriteLine(myText + moreText);
+            string color1 = "Yellow ";
+            string color2 = " Red ";
+            bool isYellow = true;
+            bool isGreen = false;
+            Console.Write(color1 + isYellow + color2 + isGreen);
+            double numbers;
+            numbers = mySecondNumber / myFirstNumber;
+            Console.WriteLine(numbers);
+            numbers = mySecondNumber / (double)myFirstNumber;
+            Console.WriteLine(numbers);
+            Console.WriteLine("Breakpoint Here");
+
+
+            string textToNumber = "80473";
+            int results = 99 * int.Parse(textToNumber);
+            Console.WriteLine(results);
+            DateTime rightNow = DateTime.Now;
+            Console.WriteLine(rightNow);
+
+
+        }
+    }
+}
